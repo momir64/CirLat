e5cffdeae87338f6d3bf2be3e8dc6c1d013c1481
Momir Stanisic <Momir.stanisic.ns@hotmail.com>
2021-03-22 16:20:28 +0100

    Dodata parcijalna transliteracija

--- RibbonController.cs
+++ RibbonController.cs
@@ -6,8 +6,8 @@ using Word = Microsoft.Office.Interop.Word;
 namespace CirLat {
     [ComVisible(true)]
     public class RibbonController : Microsoft.Office.Core.IRibbonExtensibility {
-        private string[] lat = { "A", "B", "V", "G", "D", "Đ", "E", "Ž", "Z", "I", "J", "K", "L", "Lj", "M", "N", "Nj", "O", "P", "R", "S", "T", "Ć", "U", "F", "H", "C", "Č", "Dž", "Š", "a", "b", "v", "g", "d", "đ", "e", "ž", "z", "i", "j", "k", "l", "lj", "m", "n", "nj", "o", "p", "r", "s", "t", "ć", "u", "f", "h", "c", "č", "dž", "š" };
-        private string[] cir = { "А", "Б", "В", "Г", "Д", "Ђ", "Е", "Ж", "З", "И", "Ј", "К", "Л", "Љ", "М", "Н", "Њ", "О", "П", "Р", "С", "Т", "Ћ", "У", "Ф", "Х", "Ц", "Ч", "Џ", "Ш", "а", "б", "в", "г", "д", "ђ", "е", "ж", "з", "и", "ј", "к", "л", "љ", "м", "н", "њ", "о", "п", "р", "с", "т", "ћ", "у", "ф", "х", "ц", "ч", "џ", "ш" };
+        private string[] lat = { "dž", "nj", "lj", "Dž", "Nj", "Lj", "DŽ", "NJ", "LJ", "A", "B", "V", "G", "D", "Đ", "E", "Ž", "Z", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "Ć", "U", "F", "H", "C", "Č", "Š", "a", "b", "v", "g", "d", "đ", "e", "ž", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "ć", "u", "f", "h", "c", "č", "š" };
+        private string[] cir = { "џ", "њ", "љ", "Џ", "Њ", "Љ", "Џ", "Њ", "Љ", "А", "Б", "В", "Г", "Д", "Ђ", "Е", "Ж", "З", "И", "Ј", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "Ћ", "У", "Ф", "Х", "Ц", "Ч", "Ш", "а", "б", "в", "г", "д", "ђ", "е", "ж", "з", "и", "ј", "к", "л", "м", "н", "о", "п", "р", "с", "т", "ћ", "у", "ф", "х", "ц", "ч", "ш" };
         public string GetCustomUI(string ribbonID) =>
             @"<customUI xmlns='http://schemas.microsoft.com/office/2009/07/customui'>
                         <ribbon>
@@ -21,21 +21,24 @@ namespace CirLat {
                             </tabs>
                         </ribbon>
                     </customUI>";
-        
-        public void ToCir(Microsoft.Office.Core.IRibbonControl control) {
-            Word.Range range = Globals.ThisAddIn.Application.ActiveDocument.Content;
-            range.Find.ClearFormatting();
-            for (int i = 0; i < 60; i++)
-                range.Find.Execute(FindText: lat[i], ReplaceWith: cir[i], Replace: Word.WdReplace.wdReplaceAll, MatchCase: true);
+
+        public void FindReplace(string[] find, string[] replace) {
+            bool selected = Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection.Type == Word.WdSelectionType.wdSelectionNormal;
+            Word.Range range1 = selected ? Globals.ThisAddIn.Application.ActiveDocument.ActiveWindow.Selection.Range : Globals.ThisAddIn.Application.ActiveDocument.Content;
+            range1.Find.ClearFormatting();
+            for (int i = 0; i < 63; i++)
+                range1.Find.Execute(FindText: find[i], ReplaceWith: replace[i], Replace: Word.WdReplace.wdReplaceAll, MatchCase: true);
+            if (!selected && Globals.ThisAddIn.Application.ActiveDocument.Footnotes.Count > 0) {
+                Word.Range range2 = Globals.ThisAddIn.Application.ActiveDocument.Footnotes[1].Range;
+                range2.WholeStory();
+                range2.Find.ClearFormatting();
+                for (int i = 0; i < 63; i++)
+                    range2.Find.Execute(FindText: find[i], ReplaceWith: replace[i], Replace: Word.WdReplace.wdReplaceAll, MatchCase: true);
+            }
         }
 
-        public void ToLat(Microsoft.Office.Core.IRibbonControl control) {
-            Word.Range range = Globals.ThisAddIn.Application.ActiveDocument.Content;
-            range.Find.ClearFormatting();
-            for (int i = 0; i < 60; i++)
-                range.Find.Execute(FindText: cir[i], ReplaceWith: lat[i], Replace: Word.WdReplace.wdReplaceAll, MatchCase: true);
-        }
-
+        public void ToCir(Microsoft.Office.Core.IRibbonControl control) => FindReplace(lat, cir);
+        public void ToLat(Microsoft.Office.Core.IRibbonControl control) => FindReplace(cir, lat);
         public Bitmap GetCirImg(Microsoft.Office.Core.IRibbonControl control) => Resources.cir;
         public Bitmap GetLatImg(Microsoft.Office.Core.IRibbonControl control) => Resources.lat;
     }
